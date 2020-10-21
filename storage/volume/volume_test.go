ac40b958d65c0ca2c2ba5457c1601716a6378814
alex.zhao <alex.zhao@graviti.cn>
2020-10-21 23:48:29 +0800

    finish newneedle test

--- storage/volume/volume_test.go
+++ storage/volume/volume_test.go
@@ -2,15 +2,14 @@ package volume
 
 import (
 	"github.com/stretchr/testify/assert"
-	"io/ioutil"
 	"os"
 	"testing"
 )
 
 func TestNewVolume(t *testing.T) {
 	testDir := "/Users/alex/go/src/github.com/AlexanderChiuluvB/xiaoyaoFS/test"
-	dir, _ := ioutil.TempDir(testDir, "")
-	defer os.RemoveAll(dir)
+	os.Mkdir(testDir, os.ModePerm)
+	defer os.RemoveAll(testDir)
 	v, err := NewVolume(1, testDir)
 	assert.NoError(t, err)
 	t.Logf("%+v", v)
@@ -18,13 +17,26 @@ func TestNewVolume(t *testing.T) {
 
 func TestNewVolume_NewNeedle(t *testing.T) {
 	testDir := "/Users/alex/go/src/github.com/AlexanderChiuluvB/xiaoyaoFS/test"
-	dir, _ := ioutil.TempDir(testDir, "")
-	defer os.RemoveAll(dir)
+	os.Mkdir(testDir, os.ModePerm)
+	defer os.RemoveAll(testDir)
 	v, err := NewVolume(1, testDir)
 	assert.NoError(t, err)
 	n, err := v.NewNeedle(1, "test.txt", []byte("test"))
 	defer v.DelNeedle(1)
 	assert.NoError(t, err)
+	assert.Equal(t, "test.txt", n.FileName)
+	assert.Equal(t, uint64(4), n.FileSize)
+	assert.Equal(t, uint64(8), n.NeedleOffset)
 	t.Logf("%+v", n)
+
+	n2, err := v.NewNeedle(2, "test.jpg", []byte("test"))
+	defer v.DelNeedle(2)
+	assert.NoError(t, err)
+	assert.Equal(t, "test.jpg", n2.FileName)
+	assert.Equal(t, uint64(4), n2.FileSize)
+	assert.Equal(t, uint64(60), n2.NeedleOffset)
+	t.Logf("%+v", n2)
+
+
 }
 
